b4042f4cc54be4610b3aca7edcf91a2e7d5a17df
katie <kateann58@gmail.com>
2018-09-23 13:25:51 -0400

    added invalid user input, removed id usage, added methods

--- Lab1Part1/Lab1Part1/Lab1Part1/Program.cs
+++ Lab1Part1/Lab1Part1/Lab1Part1/Program.cs
@@ -10,27 +10,57 @@ namespace Lab1Part1
     {
         static void Main(string[] args)
         {
-        Start:
 
-            int nmbr1; //input #1
-            int nmbr2; //input #2
+            int input; //placeholder for user input
+            int nmbr1 = 0; //input #1
+            int nmbr2 = 0; //input #2
+
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                Console.Clear();
+                //set values from user input
+                Directions();
+                Console.WriteLine("Please provide two numbers...");
+                Console.WriteLine("Number 1:  ");
+                if (int.TryParse(Console.ReadLine(), out input)) //checks for valid input
+                {
+                    nmbr1 = input;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR - Invalid Input");
+                    Retry();
+                    Console.ReadKey();
+                    continue;
+                }
+                Console.WriteLine("Number 2:  ");
+                if (int.TryParse(Console.ReadLine(), out input))
+                {
+                    nmbr2 = input;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR - Invalid Input");
+                    Retry();
+                    Console.ReadKey();
+                    continue;
+                }
+                CalculateSum(nmbr1, nmbr2);
+                Retry();
+                Console.ReadKey();
+            }
+        } //**********end main method**********
+        static Boolean CalculateSum(int nmbr1, int nmbr2)
+        {
             int value1; //place value of #1
             int value2; //place value of #2
-
-            //set values from user input
-            Directions();
-            Console.WriteLine("Please provide two numbers...");
-            Console.WriteLine("Number 1:  ");
-            nmbr1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Number 2:  ");
-            nmbr2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Environment.NewLine);
 
             //get place values
             value1 = nmbr1.ToString().Length;
             value2 = nmbr2.ToString().Length;
 
             //display place value
+            Console.WriteLine(Environment.NewLine);
             Console.WriteLine("place value of first: {0}", value1);
             Console.WriteLine("place value of second: {0}", value2);
 
@@ -44,47 +74,38 @@ namespace Lab1Part1
                 Console.WriteLine("Place Values are equal");
                 Console.WriteLine(Environment.NewLine);
                 Console.WriteLine("corresponding digits added together: ");
-                for (int i = 0, j = 0; i < value1; i++, j++)
-                {
-                    sum[i] = ray1[i] + ray2[j]; //add digits into new array
-                }
                 for (int i = 0; i < value1; i++)
                 {
+                    sum[i] = ray1[i] + ray2[i]; //add digits into new array
                     Console.Write(sum[i] + " "); //show new digits
                 }
-
                 //compare digits 
                 Console.WriteLine(Environment.NewLine);
-                Console.WriteLine("are the sum of each corresponding digits the same?");
+                Console.WriteLine("are the sum of the corresponding digits the same?");
                 Console.Write(Array.TrueForAll(sum, i => i == sum[0]));
             }
-
             else
             {
                 Console.Write("***Please provide numbers with equal place values***");
             }
-
+            return true;
+        }
+        static Boolean Retry()
+        {
             Console.WriteLine(Environment.NewLine);
             Console.WriteLine("Press SPACEBAR to retry");
-
-            Console.ReadKey();
-
-            //restart program
             while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar))
             {
-                Console.Clear();
-                goto Start;
+                return false;
             }
-            Console.ReadKey();
+            return false;
         }
-        //end main method
-        //new method containing user directions
         static void Directions()
         {
-            Console.WriteLine("*numbers must have equal place values*");
-            Console.WriteLine("     (example: xxx & yyy both have 3 digits and are equal in length)");
+            Console.WriteLine("                  *numbers must have equal place values*");
+            Console.WriteLine("        (example: xxx & yyy both have 3 digits and are equal in length)");
             Console.WriteLine("*digits will be added to corresponding digits and the sum of each must be equal*");
-            Console.WriteLine("     (example: for abc & xyz, ax=by=cz)");
+            Console.WriteLine("                    (example: for abc & xyz, ax=by=cz)");
             Console.WriteLine(Environment.NewLine);
         }
     }
